8db9caa2a4e3e36ca982ba8e5d3b880634ccc0ea
Steffkn <steff.kn@abv.bg>
2018-01-29 23:33:28 +0200

    Added: LDS-ReversedList

---
+++ 02. LDS-ReversedList/ReversedList/Launcher.cs
@@ -0,0 +1,42 @@
+﻿namespace ReversedList
+{
+    using System;
+
+    public class Launcher
+    {
+        public static void Main()
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            var list = new ReversedList<int>();
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Add(7);
+
+            Console.WriteLine("Count " + list.Count);
+            Console.WriteLine("Capacity" + list.Capacity);
+
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+            list.RemoveAt(7);
+
+            Console.WriteLine("Count " + list.Count);
+            Console.WriteLine("Capacity " + list.Capacity);
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+
+}
---
+++ 02. LDS-ReversedList/ReversedList/ReversedList.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class ReversedList<T> : IEnumerable<T>
+{
+    private const int DefaulthCapasity = 2;
+    private T[] items;
+    private int capacity;
+
+    public ReversedList(int capasity = DefaulthCapasity)
+    {
+        this.items = new T[capasity];
+        this.Capacity = capasity;
+    }
+
+    public int Count { get; set; }
+
+    public int Capacity { get => this.items.Length; private set => this.capacity = value; }
+
+    public T this[int index]
+    {
+        get
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return this.items[this.Count - index - 1];
+        }
+
+        set
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            this.items[this.Count - index - 1] = value;
+        }
+    }
+
+    public void Add(T item)
+    {
+        if (this.items.Length == this.Count)
+        {
+            this.Resize();
+        }
+
+        this.items[this.Count] = item;
+        this.Count++;
+    }
+
+    public T RemoveAt(int index)
+    {
+        if (index >= this.Count || index < 0)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        T element = this.items[index];
+        this.items[index] = default(T);
+        this.Shift(index);
+        this.Count--;
+
+        if (this.Count <= this.items.Length / 4)
+        {
+            this.Shrink();
+        }
+
+        return element;
+    }
+
+    private void Shrink()
+    {
+        T[] copy = new T[this.items.Length / 2];
+        for (int i = 0; i < this.Count; i++)
+        {
+            copy[i] = this.items[i];
+        }
+        this.items = copy;
+    }
+
+    private void Shift(int index)
+    {
+        for (int i = this.Count - index - 1; i < this.Count - 1; i++)
+        {
+            this.items[i] = this.items[i + 1];
+        }
+    }
+
+    private void Resize()
+    {
+        T[] copy = new T[this.items.Length * 2];
+
+        for (int i = 0; i < this.items.Length; i++)
+        {
+            copy[i] = this.items[i];
+        }
+        this.items = copy;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = this.Count - 1; i >= 0; i--)
+        {
+            yield return this.items[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
