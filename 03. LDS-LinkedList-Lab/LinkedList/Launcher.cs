46c3941a1fd93cb11ade1ffe18f0af2049d492e0
Steffkn <steff.kn@abv.bg>
2018-01-29 23:34:06 +0200

    Added: LDS-LinkedList-Lab

---
+++ 03. LDS-LinkedList-Lab/LinkedList/Launcher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+class Launcher
+{
+    public static void Main()
+    {
+        LinkedList<int> list = new LinkedList<int>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            list.AddFirst(i);
+            Console.WriteLine("{0} {1}", i, list.Head.Value);
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("{0} {1}", i, list.RemoveLast());
+        }
+
+        Console.WriteLine("{0} {1}", 0, list.Count);
+    }
+}
---
+++ 03. LDS-LinkedList-Lab/LinkedList/LinkedList.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LinkedList<T> : IEnumerable<T>
+{
+    public class Node
+    {
+        public Node(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public Node Next { get; set; }
+    }
+
+    public Node Head { get; private set; }
+
+    public Node Tail { get; private set; }
+
+    public int Count { get; private set; }
+
+    public void AddFirst(T item)
+    {
+        if (this.Count == 0)
+        {
+            this.Head = new Node(item);
+            this.Tail = new Node(item);
+        }
+        else
+        {
+            var second = new Node(this.Head.Value);
+            second.Next = this.Head.Next;
+
+            this.Head.Value = item;
+            this.Head.Next = second;
+        }
+
+        this.Count++;
+    }
+
+    public void AddLast(T item)
+    {
+        if (this.Count == 0)
+        {
+            this.Head = new Node(item);
+            this.Tail = Head;
+        }
+        else
+        {
+            var newTailNode = new Node(item);
+            this.Tail.Next = newTailNode;
+            this.Tail = newTailNode;
+        }
+
+        this.Count++;
+    }
+
+    public T RemoveFirst()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        var value = this.Head.Value;
+        this.Head = this.Head.Next;
+        this.Count--;
+        return value;
+    }
+
+    public T RemoveLast()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        var value = this.Tail.Value;
+        var newTailNode = this.GetSecondToLast();
+        newTailNode.Next = null;
+
+        this.Tail = newTailNode;
+        this.Count--;
+        return value;
+    }
+
+    private Node GetSecondToLast()
+    {
+        var current = this.Head;
+        var secondToLast = this.Head;
+
+        while (current.Next != null)
+        {
+            secondToLast = current;
+            current = current.Next;
+        }
+
+        return secondToLast;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        var current = this.Head;
+
+        while (current != null)
+        {
+            var result = current.Value;
+            current = current.Next;
+            yield return result;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
