a542c0ecd9927024ab6080c4821451125854d6bb
Steffkn <steff.kn@abv.bg>
2018-03-08 01:04:23 +0200

    Added: DS-Ropes TDS-Trie Added: DS-Ropes TextEditor

---
+++ 14.DS-Ropes-Tries-Exercise/TextEditor/TextEditor/StartUp.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ITextEditor textEditor = new TextEditor();
+
+        Regex regex = new Regex("(?<=\")(?<text>.*)(?=\")");
+
+        string input = string.Empty;
+
+        while ((input = Console.ReadLine()) != "end")
+        {
+            Match matchText = regex.Match(input);
+
+            string[] tokens = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            switch (tokens[0])
+            {
+                case "login":
+                    string userName = tokens[1];
+                    textEditor.Login(userName);
+                    break;
+                case "logout":
+                    userName = tokens[1];
+                    textEditor.Logout(userName);
+                    break;
+                case "users":
+                    List<string> users;
+                    if (tokens.Length > 1)
+                    {
+                        users = textEditor.Users(tokens[1]).ToList();
+                    }
+                    else users = textEditor.Users().ToList();
+                    PrintUsers(users);
+                    break;
+                default:
+                    userName = tokens[0];
+
+                    string text = matchText.Groups["text"].Value;
+
+                    switch (tokens[1])
+                    {
+                        case "insert":
+                            if (!matchText.Success) continue;
+                            textEditor.Insert(userName, int.Parse(tokens[2]), text);
+                            break;
+                        case "prepend":
+                            if (!matchText.Success) continue;
+                            textEditor.Prepend(userName, text);
+                            break;
+                        case "substring":
+                            textEditor.Substring(userName, int.Parse(tokens[2]), int.Parse(tokens[3]));
+                            break;
+                        case "delete":
+                            textEditor.Delete(userName, int.Parse(tokens[2]), int.Parse(tokens[3]));
+                            break;
+                        case "clear":
+                            textEditor.Clear(userName);
+                            break;
+                        case "length":
+                            Console.WriteLine(textEditor.Length(userName));
+                            break;
+                        case "print":
+                            var printResult = textEditor.Print(userName);
+                            if (printResult != null)
+                                Console.WriteLine(printResult);
+                            break;
+                        case "undo":
+                            textEditor.Undo(userName);
+                            break;
+                    }
+                    break;
+            }
+        }
+    }
+
+
+    private static void PrintUsers(List<string> users)
+    {
+        foreach (var user in users)
+        {
+            Console.WriteLine(user);
+        }
+    }
+}
---
+++ 14.DS-Ropes-Tries-Exercise/TextEditor/TextEditor/TextEditor.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+public class TextEditor : ITextEditor
+{
+    private Trie<BigList<char>> usersStrings;
+
+    private Trie<Stack<BigList<char>>> usersStack;
+
+    public TextEditor()
+    {
+        this.usersStrings = new Trie<BigList<char>>();
+        this.usersStack = new Trie<Stack<BigList<char>>>();
+    }
+
+    public void Clear(string username)
+    {
+        if (!this.usersStrings.Contains(username))
+        {
+            return;
+        }
+        this.usersStack.GetValue(username).Push(new BigList<char>(this.usersStrings.GetValue(username)));
+        this.usersStrings.Insert(username, new BigList<char>());
+    }
+
+    public void Delete(string username, int startIndex, int length)
+    {
+        if (!this.usersStrings.Contains(username))
+        {
+            return;
+        }
+
+        this.usersStack.GetValue(username).Push(new BigList<char>(this.usersStrings.GetValue(username)));
+
+        this.usersStrings.GetValue(username).RemoveRange(startIndex, length);
+    }
+
+    public void Insert(string username, int index, string text)
+    {
+        if (!this.usersStrings.Contains(username))
+        {
+            return;
+        }
+
+        this.usersStack.GetValue(username).Push(new BigList<char>(this.usersStrings.GetValue(username)));
+
+        this.usersStrings.GetValue(username).InsertRange(index, text);
+    }
+
+    public int Length(string username)
+    {
+        if (!this.usersStrings.Contains(username))
+        {
+            return 0;
+        }
+
+        return this.usersStrings.GetValue(username).Count();
+    }
+
+    public void Login(string username)
+    {
+        this.usersStrings.Insert(username, new BigList<char>());
+        this.usersStack.Insert(username, new Stack<BigList<char>>());
+    }
+
+    public void Logout(string username)
+    {
+        this.usersStrings.Delete(username);
+        this.usersStack.Delete(username);
+    }
+
+    public void Prepend(string username, string text)
+    {
+        if (!this.usersStrings.Contains(username))
+        {
+            return;
+        }
+        this.usersStack.GetValue(username).Push(this.PrintBigList(username));
+        this.usersStrings.GetValue(username).AddRangeToFront(text);
+    }
+
+    public string Print(string username)
+    {
+        if (!this.usersStrings.Contains(username))
+        {
+            return null;
+        }
+
+        return string.Join("", this.PrintBigList(username));
+    }
+
+    private BigList<char> PrintBigList(string username)
+    {
+        if (!this.usersStrings.Contains(username))
+        {
+            return new BigList<char>();
+        }
+
+        var list = this.usersStrings.GetValue(username);
+
+        return new BigList<char>(list);
+    }
+
+    public void Substring(string username, int startIndex, int length)
+    {
+        if (!this.usersStrings.Contains(username))
+        {
+            return;
+        }
+
+        var userString = this.usersStrings.GetValue(username);
+
+        var userStack = this.usersStack.GetValue(username);
+
+        userStack.Push(new BigList<char>(userString));
+
+        var newString = new BigList<char>();
+
+        int endIndex = startIndex + length;
+        startIndex = Math.Max(0, startIndex);
+        endIndex = Math.Min(userString.Count, endIndex);
+
+        for (int i = startIndex; i < endIndex; i++)
+        {
+            newString.Add(userString[i]);
+        }
+
+        this.usersStrings.Insert(username, newString);
+    }
+
+    public void Undo(string username)
+    {
+        if (!this.usersStrings.Contains(username))
+        {
+            return;
+        }
+        Environment.Exit(0);
+        var userStack = this.usersStack.GetValue(username);
+
+        if (userStack.Count == 0) return;
+
+        var lastString = userStack.Pop();
+
+        this.usersStrings.Insert(username, new BigList<char>(lastString));
+    }
+
+    public IEnumerable<string> Users(string prefix = "")
+    {
+        Queue<string> queue = new Queue<string>();
+
+        foreach (var user in this.usersStrings.GetByPrefix(prefix))
+        {
+            queue.Enqueue(user);
+        }
+
+        return queue;
+    }
+}
