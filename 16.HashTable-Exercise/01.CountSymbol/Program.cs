3eae822a9046e099aaa83f9bd0cb8816733d5e26
Steffkn <steff.kn@abv.bg>
2018-03-08 01:05:41 +0200

    Added: HashTable-Lab Added: HashTable-Exercise

---
+++ 16.HashTable-Exercise/01.CountSymbol/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    static void Main()
+    {
+        var input = Console.ReadLine();
+        HashTable<char, int> table = new HashTable<char, int>();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (!table.ContainsKey(input[i]))
+            {
+                table.Add(input[i], 0);
+            }
+
+            table[input[i]]++;
+        }
+
+        foreach (var ch in table.OrderBy(t => t.Key))
+        {
+            Console.WriteLine($"{ch.Key}: {ch.Value} time/s");
+        }
+    }
+}
---
+++ 16.HashTable-Exercise/02.Phonebook/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var phoneNumbers = new HashTable<string, string>();
+
+        string input = string.Empty;
+        while ((input = Console.ReadLine()) != "search")
+        {
+            var phoneArgs = input.Split(new char[] { '-' }, StringSplitOptions.RemoveEmptyEntries);
+            if (!phoneNumbers.ContainsKey(phoneArgs[0]))
+            {
+                phoneNumbers[phoneArgs[0]] = phoneArgs[1];
+            }
+        }
+
+        while ((input = Console.ReadLine()) != "end")
+        {
+            if (!phoneNumbers.ContainsKey(input))
+            {
+                Console.WriteLine($"Contact {input} does not exist.");
+            }
+            else
+            {
+                Console.WriteLine($"{input} -> {phoneNumbers[input]}");
+            }
+        }
+    }
+}
