d458f4d4b8991131e2bf0555a0e1a045272a965c
Steffkn <steff.kn@abv.bg>
2018-03-08 01:03:28 +0200

    Added: AVLTrees First-Last-List

---
+++ 13.DS-AVLTrees-AATrees-Exercise/First-Last-List/First-Last-List/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wintellect.PowerCollections;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // Arrange
+        var items = FirstLastListFactory.Create<Product>();
+        items.Add(new Product(0.50m, "coffee"));
+        items.Add(new Product(1.20m, "mint drops"));
+        items.Add(new Product(1.20m, "beer"));
+        items.Add(new Product(0.50m, "candy"));
+        items.Add(new Product(1.20m, "cola"));
+
+        // Act
+        var returnedItems = items.Max(4).Select(p => p.Title).ToList();
+
+        // Assert
+        var expectedItems = new string[] {
+            "mint drops", "beer", "cola", "coffee" };
+
+        foreach (var returnedItem in returnedItems)
+        {
+            Console.WriteLine(returnedItem);
+        }
+        Console.WriteLine();
+
+        // Assert
+    }
+    class Product : IComparable<Product>
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+
+        public Product(decimal price, string title)
+        {
+            this.Price = price;
+            this.Title = title;
+        }
+
+        public int CompareTo(Product other)
+        {
+            return this.Price.CompareTo(other.Price);
+
+        }
+    }
+
+}
